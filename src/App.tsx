f6758126eb66702f0032dc7bf02442b05269c63b
lorde <rioaformyking@gmail.com>
2022-06-07 18:52:48 +0800

    add:base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
+
 import Home from './pages/Home'
 import Sidebar from './components/Sidebar'
+import * as AiIcon from 'react-icons/ai';
+
+import './App.css';
 
 function App() {
 
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = function () {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+  }
+
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       <Sidebar />
-      {/* <Home/> */}
+
+      <div className='Main flex flex-col'>
+        <div className="navbar flex w-full">
+          <button className='toggle-icon w-6 h-6 m-3' onClick={toggle}>
+            <AiIcon.AiOutlineMenu className='text-gray-600 w-6 h-6' />
+          </button>
+
+        </div>
+
+
+        <div className='grow'><p>Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content 
+        Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content </p></div>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,19 +4,18 @@ import * as AiIcon from 'react-icons/ai';
 import './Sidebar.css';
 
 function Sidebar() {
-    const [isOpen, setIsOpen] = useState(false);
-    const toggle = () => setIsOpen(!isOpen);
 
     return (
+            
         <>
-            <div className="sidebar flex flex-col h-screen">
+            <div className="sidebar flex flex-col w-50 h-full">
                 <div className="sidebar-header flex justify-center items-center m-3">
                     <div className="avatar w-9 h-9">
                         <img src="https://avatars.githubusercontent.com/u/74342663" alt="" className=' rounded-full' />
                     </div>
                     <span className='mx-2 mr-9'>Username</span>
-                    <button className="leftToggle ">
-                        <AiIcon.AiOutlineDoubleLeft className=' text-gray-600 w-6 h-6' onClick={toggle} />
+                    <button className="leftToggle">
+                        <AiIcon.AiOutlineDoubleLeft className=' text-gray-600 w-6 h-6'/>
                     </button>
 
                 </div>
